bb3776f6d63e391fbb395eece6f79a9bd0b750ee
enesify <enesbatur@protonmail.com>
2018-12-21 15:56:49 +0300

    project moved to springDemoOne folder

---
+++ springDemoOne/src/main/java/com/enes/springdemo/TrackCoach.java
@@ -0,0 +1,22 @@
+package com.enes.springdemo;
+
+public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "Just Do It: " + fortuneService.getFortune();
+	}
+
+}
